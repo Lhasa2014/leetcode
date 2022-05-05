35812c6670d94dcf88bbba089f3c5721defaf507
Lhasa2014 <jieyang_xaut@yahoo.com>
2022-05-05 22:16:22 +0800

    713. 乘积小于 K 的子数组 | M

---
+++ 713.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int res = 0;
+        int start = 0, cur = 0, end = nums.size();
+        int mul = 1;
+        while (cur < end) {
+            if (nums[cur] >= k) {
+                // int tmp = 0;
+                for (int i = 1; i <= cur-start; ++i) {
+                    // tmp += i;
+                    res += i;
+                }
+                // res += tmp;
+                start = cur + 1;
+                mul = 1;
+            } else {
+                mul *= nums[cur];
+                while (mul >= k) {
+                    mul /= nums[start];
+                    res += cur - start;
+                    start++;
+                }
+            }
+            cur++;
+        }
+        for (int i = 1; i <= cur-start; ++i) {
+            res += i;
+        }
+        return res;
+    }
+};
